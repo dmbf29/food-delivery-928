f93cb46c502bd77291150ec4d278edcf2cbba6e6
Douglas Berkley <douglasmberkley@gmail.com>
2022-07-20 10:07:07 +0900

    added one coment to order

--- app/repositories/order_repository.rb
+++ app/repositories/order_repository.rb
@@ -5,6 +5,7 @@ class OrderRepository
     CSV.foreach(@csv_file_path, headers: :first_row, header_converters: :symbol) do |attributes|
       # attributes -> create an instance of a order
       attributes[:id] = attributes[:id].to_i
+      # we need to convert delivered to a boolean
       # we need to ask the meal_repo for a meal the meal_id
       # we need to ask the customer_repo for a customer the customer_id
       # we need to ask the employee_repo for a employee the employee_id
